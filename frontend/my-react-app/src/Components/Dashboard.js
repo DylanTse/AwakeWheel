f7b54c4368881d8982fcd598aa2b60d2c09dcd7f
DylanTse <dylan.tse.1011@gmail.com>
2024-03-23 17:01:13 -0500

    design upgrade

--- frontend/my-react-app/src/Components/Dashboard.js
+++ frontend/my-react-app/src/Components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import "./Dashboard.css"
 
 function Dashboard() {
   return (
     <div>
-      <h2>Home Screen</h2>
-      <p>Welcome to the Home Screen!</p>
+      <h2>Dashboard</h2>
+      <img className="image" src={'https://th.bing.com/th/id/R.07852f228cc629c0bb6c3625e7c98d6f?rik=N%2fZ5uZsCTRS1gQ&riu=http%3a%2f%2fclipart-library.com%2fimg%2f1121340.png&ehk=hn6FqGOrEXe%2fYh2E1KxSlIlg5gQbDuSxAaeqWdz34Mc%3d&risl=&pid=ImgRaw&r=0'}></img>
+      <img className='image' src={'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b2001a68-c2ef-41c2-9e5c-78407b5224cf/dgoma0o-e6603229-91b2-4ab9-8ee9-fdc063914935.png/v1/fill/w_512,h_512/mater_cars_png_by_naufalisback_dgoma0o-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTEyIiwicGF0aCI6IlwvZlwvYjIwMDFhNjgtYzJlZi00MWMyLTllNWMtNzg0MDdiNTIyNGNmXC9kZ29tYTBvLWU2NjAzMjI5LTkxYjItNGFiOS04ZWU5LWZkYzA2MzkxNDkzNS5wbmciLCJ3aWR0aCI6Ijw9NTEyIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.vsv_F6KT2iEt1cmx1pT4BQ6b2B7Q3mBca4TtR2C9i54'}></img>
     </div>
   );
 }
